adf86bd7754f0f3a9ce77acbbe0e8f11a7b4dd3d
Fateme Esmaili <fateme.e7896@gmail.com>
2022-04-15 15:34:37 +0430

    q2 added

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
-function lastElement(arr, n){
-    if (n == null){
-        console.log(arr[arr.length - 1]);
-    }else if(n > arr.length){
-        console.log(arr)
-    }
-    else{
-        let res = arr.slice(arr.length - n);
-        console.log(res);
-    }
+// function lastElement(arr, n){
+//     if (n == null){
+//         console.log(arr[arr.length - 1]);
+//     }else if(n > arr.length){
+//         console.log(arr)
+//     }
+//     else{
+//         let res = arr.slice(arr.length - n);
+//         console.log(res);
+//     }
+// }
+
+
+// lastElement([2, 6, 0, -2, 4]);
+// lastElement([2, 6, 0, -2, 4],3);
+// lastElement([2, 6, 0, -2, 4],6);
+
+
+function joinElements(arr){
+    let res = arr.join(',');
+    console.log(`" ${res} "`);
+
+    let res2 = arr.join('+');
+    console.log(`" ${res2} "`);
 }
-
-
-lastElement([2, 6, 0, -2, 4]);
-lastElement([2, 6, 0, -2, 4],3);
-lastElement([2, 6, 0, -2, 4],6);
+joinElements(["Red", "Green", "White", "Black"]);
